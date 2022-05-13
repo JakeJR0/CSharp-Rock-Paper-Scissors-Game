157ecdbfde2b4d25f646416d623c6c4b3096c04c
Jake JR <1453194@stu.tmc.ac.uk>
2022-05-13 13:24:18 +0100

    Add project files.

---
+++ Rock Paper Scissors/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Rock_Paper_Scissors
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userChoiceLabel = new System.Windows.Forms.Label();
+            this.computerChoiceLabel = new System.Windows.Forms.Label();
+            this.computerScoreLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.rockButton = new System.Windows.Forms.Button();
+            this.paperButton = new System.Windows.Forms.Button();
+            this.scissorButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(465, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem,
+            this.exitGameToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // exitGameToolStripMenuItem
+            // 
+            this.exitGameToolStripMenuItem.Name = "exitGameToolStripMenuItem";
+            this.exitGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitGameToolStripMenuItem.Text = "Exit Game";
+            this.exitGameToolStripMenuItem.Click += new System.EventHandler(this.exitGameToolStripMenuItem_Click);
+            // 
+            // userChoiceLabel
+            // 
+            this.userChoiceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.userChoiceLabel.Location = new System.Drawing.Point(12, 244);
+            this.userChoiceLabel.Name = "userChoiceLabel";
+            this.userChoiceLabel.Size = new System.Drawing.Size(441, 32);
+            this.userChoiceLabel.TabIndex = 1;
+            this.userChoiceLabel.Text = "Your Choice: ";
+            this.userChoiceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // computerChoiceLabel
+            // 
+            this.computerChoiceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.computerChoiceLabel.Location = new System.Drawing.Point(22, 197);
+            this.computerChoiceLabel.Name = "computerChoiceLabel";
+            this.computerChoiceLabel.Size = new System.Drawing.Size(431, 39);
+            this.computerChoiceLabel.TabIndex = 2;
+            this.computerChoiceLabel.Text = "Computer Choice: ";
+            this.computerChoiceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // computerScoreLabel
+            // 
+            this.computerScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.computerScoreLabel.Location = new System.Drawing.Point(17, 141);
+            this.computerScoreLabel.Name = "computerScoreLabel";
+            this.computerScoreLabel.Size = new System.Drawing.Size(436, 38);
+            this.computerScoreLabel.TabIndex = 3;
+            this.computerScoreLabel.Text = "Computer Score: 0";
+            this.computerScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.scoreLabel.Location = new System.Drawing.Point(17, 102);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(436, 37);
+            this.scoreLabel.TabIndex = 4;
+            this.scoreLabel.Text = "Your Score: 0";
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Title
+            // 
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.Title.Location = new System.Drawing.Point(27, 28);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(426, 49);
+            this.Title.TabIndex = 5;
+            this.Title.Text = "Rock Paper Scissors";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rockButton
+            // 
+            this.rockButton.BackColor = System.Drawing.Color.SkyBlue;
+            this.rockButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rockButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.rockButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.rockButton.Location = new System.Drawing.Point(34, 369);
+            this.rockButton.Name = "rockButton";
+            this.rockButton.Size = new System.Drawing.Size(110, 35);
+            this.rockButton.TabIndex = 6;
+            this.rockButton.Text = "Rock";
+            this.rockButton.UseVisualStyleBackColor = false;
+            this.rockButton.Click += new System.EventHandler(this.rockButton_Click);
+            // 
+            // paperButton
+            // 
+            this.paperButton.BackColor = System.Drawing.Color.SkyBlue;
+            this.paperButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.paperButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.paperButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.paperButton.Location = new System.Drawing.Point(182, 369);
+            this.paperButton.Name = "paperButton";
+            this.paperButton.Size = new System.Drawing.Size(110, 35);
+            this.paperButton.TabIndex = 7;
+            this.paperButton.Text = "Paper";
+            this.paperButton.UseVisualStyleBackColor = false;
+            this.paperButton.Click += new System.EventHandler(this.paperButton_Click);
+            // 
+            // scissorButton
+            // 
+            this.scissorButton.BackColor = System.Drawing.Color.SkyBlue;
+            this.scissorButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.scissorButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.scissorButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.scissorButton.Location = new System.Drawing.Point(332, 369);
+            this.scissorButton.Name = "scissorButton";
+            this.scissorButton.Size = new System.Drawing.Size(110, 35);
+            this.scissorButton.TabIndex = 8;
+            this.scissorButton.Text = "Scissor";
+            this.scissorButton.UseVisualStyleBackColor = false;
+            this.scissorButton.Click += new System.EventHandler(this.scissorButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.resultLabel.Location = new System.Drawing.Point(22, 293);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(431, 42);
+            this.resultLabel.TabIndex = 9;
+            this.resultLabel.Text = "Result: None";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(465, 446);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.scissorButton);
+            this.Controls.Add(this.paperButton);
+            this.Controls.Add(this.rockButton);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.computerScoreLabel);
+            this.Controls.Add(this.computerChoiceLabel);
+            this.Controls.Add(this.userChoiceLabel);
+            this.Controls.Add(this.menuStrip);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "App";
+            this.Text = "Rock Paper Scissors Game";
+            this.Load += new System.EventHandler(this.App_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitGameToolStripMenuItem;
+        private System.Windows.Forms.Label userChoiceLabel;
+        private System.Windows.Forms.Label computerChoiceLabel;
+        private System.Windows.Forms.Label computerScoreLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button rockButton;
+        private System.Windows.Forms.Button paperButton;
+        private System.Windows.Forms.Button scissorButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Rock Paper Scissors/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rock_Paper_Scissors
+{
+    public partial class App : Form
+    {
+        int userScore = 0;
+        int computerScore = 0;
+        Random computer = new Random();
+        string userChoice = "";
+        string computerChoice = "";
+        bool showMessage = false;
+
+        List<string> options = new List<string>()
+        {
+            "Rock",
+            "Paper",
+            "Scissor"
+        };
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void setResult(string result, bool allowShowing=true)
+        {
+            resultLabel.Text = $"Result: {result}";
+            if (showMessage == true && allowShowing == true)
+            {
+                MessageBox.Show(result, "Rock Paper Scissors Game");
+            }
+        }
+        private bool setUserChoice(string choice = "")
+        {
+            if (choice == "")
+            {
+                return false;
+            }
+
+            userChoiceLabel.Text = $"User Choice: {choice}";
+            userChoice = choice;
+            return true;
+        }
+
+        private bool setComputerChoice(string choice = "")
+        {
+            if (choice == "")
+            {
+                return false;
+            }
+
+            computerChoiceLabel.Text = $"Computer Choice: {choice}";
+            computerChoice = choice;
+            return true;
+        }
+
+        private void setUserScore(int score=0)
+        {
+            userScore = score;
+            scoreLabel.Text = $"Your Score: {score}";
+        }
+
+        private void setComputerScore(int score=0)
+        {
+            computerScore = score;
+            computerScoreLabel.Text = $"Computer Score: {score}";
+        }
+
+        private string getComputerChoice()
+        {
+            int choiceIndex = computer.Next(options.Count);
+            string choice = options[choiceIndex];
+            return choice;
+        }
+
+        private void incrementUserScore()
+        {
+            int new_score = userScore + 1;
+            setUserScore(new_score);
+        }
+        
+        private void incrementComputerScore()
+        {
+            int new_score = computerScore + 1;
+            setComputerScore(new_score);
+        }
+
+        private void resetGame(bool show=false)
+        {
+            if (show == true)
+            {
+                string gameName = "Rock Paper Scissors Game";
+                int diffrence = Math.Abs(userScore - computerScore);
+
+                if (userScore > computerScore)
+                {
+                    MessageBox.Show($"You won against the computer during the round with a diffrence of {diffrence}", gameName);
+                } else if (userScore < computerScore)
+                {
+                    MessageBox.Show($"You lost against the computer during the round with a diffrence of {diffrence}", gameName);
+                } else
+                {
+                    MessageBox.Show($"You draw against the computer during the round with a diffrence of {diffrence}", gameName);
+                }
+                
+            }
+
+            setUserScore(0);
+            setComputerScore(0);
+            setResult("None", false);
+            setUserChoice("Not Selected");
+            setComputerChoice("Not Selected");
+        }
+
+        private void exitGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            resetGame();
+        }
+
+        private void App_Load(object sender, EventArgs e)
+        {
+            resetGame();
+        }
+
+        private void playGame(string option)
+        {
+            string selectedOption = getComputerChoice();
+
+            setComputerChoice(selectedOption);
+            setUserChoice(option);
+
+            option = option.ToLower();
+            selectedOption = selectedOption.ToLower();
+
+            /*
+             * 0: Not Selected
+             * 1: Draw
+             * 2: Loss
+             * 3: Win
+            */
+
+
+            switch (selectedOption)
+            {
+                case "rock" when option == "rock":
+                    {
+                        setResult("You drew with the computer.");
+                        return;
+                    }
+                case "rock" when option == "paper": {
+                        setResult("You won against the computer.");
+                        incrementUserScore();
+                        return;
+                    }
+                case "rock" when option == "scissor": {
+                        setResult("You lost to the computer.");
+                        incrementComputerScore();
+                        return;
+                    }
+                case "paper" when option == "rock":
+                    {
+                        setResult("You lost to the computer.");
+                        incrementComputerScore();
+                        return;
+                    }
+                case "paper" when option == "paper":
+                    {
+                        setResult("You drew with the computer.");
+                        return;
+                    }
+                case "paper" when option == "scissor":
+                    {
+                        setResult("You won against the computer.");
+                        incrementUserScore();
+                        return;
+                    }
+                case "scissor" when option == "rock":
+                    {
+                        setResult("You won against the computer.");
+                        incrementUserScore();
+                        return;
+                    }
+                case "scissor" when option == "paper":
+                    {
+                        setResult("You lost to the computer.");
+                        incrementComputerScore();
+                        return;
+                    }
+                case "scissor" when option == "scissor":
+                    {
+                        setResult("You drew with the computer.");
+                        return;
+                    }
+            }
+        }
+        private void rockButton_Click(object sender, EventArgs e)
+        {
+            playGame("Rock");
+        }
+
+        private void paperButton_Click(object sender, EventArgs e)
+        {
+            playGame("Paper");
+        }
+
+        private void scissorButton_Click(object sender, EventArgs e)
+        {
+            playGame("Scissor");
+        }
+    }
+}
